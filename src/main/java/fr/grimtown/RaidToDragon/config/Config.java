c186f2ccde158b4ae51031f40f671b833271ea7b
BakaAless <44125445+BakaAless@users.noreply.github.com>
2021-10-11 19:57:02 +0200

    🚧 Add an initialisation method

--- src/main/java/fr/grimtown/RaidToDragon/config/Config.java
+++ src/main/java/fr/grimtown/RaidToDragon/config/Config.java
@@ -1,5 +1,13 @@
 package fr.grimtown.RaidToDragon.config;
 
+import fr.grimtown.RaidToDragon.utils.FileUtil;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class Config {
 
     private static Config instance;
@@ -13,6 +21,31 @@ public class Config {
     private boolean debug = true;
 
     private Config() {
+    }
+
+    public void init(final String folder, final String fileName) {
+        final File file = new File(folder, fileName);
+        try {
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            final YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+            for (final Field field : this.getClass().getDeclaredFields()) {
+                final String path = field.getName().replace("_", ".").replace("$", "-");
+                if (Modifier.isTransient(field.getModifiers()) || Modifier.isStatic(field.getModifiers()))
+                    continue;
+                if (Modifier.isFinal(field.getModifiers())) {
+                    config.set(path, field.get(this));
+                    continue;
+                }
+                if (!FileUtil.addDefault(config, path, field.get(this)))
+                    field.set(this, config.get(path));
+            }
+            config.save(file);
+        } catch (IOException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean hasDebugMode() {
