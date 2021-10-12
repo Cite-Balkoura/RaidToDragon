d0759e676497e95111e8d90dba70f0fb38c0e9c7
BakaAless <44125445+BakaAless@users.noreply.github.com>
2021-10-12 13:50:08 +0200

    🚧 Introduce resurrection system

--- src/main/java/fr/grimtown/RaidToDragon/entities/GamePlayer.java
+++ src/main/java/fr/grimtown/RaidToDragon/entities/GamePlayer.java
@@ -6,22 +6,38 @@ public class GamePlayer {
 
     private final UUID uniqueId;
 
+    private boolean alreadyRevive;
     private double enderDamage;
 
     private long startTime;
+    private long firstDeathTime;
+    private long resurrectTime;
     private long deadTime;
     private long portalEndTime;
 
     public GamePlayer(final UUID uniqueId) {
         this.uniqueId = uniqueId;
+        this.alreadyRevive = false;
         this.enderDamage = 0;
         this.startTime = -1;
         this.deadTime = -1;
         this.portalEndTime = -1;
     }
 
+    public void resurrect(final GamePlayer doctor) {
+        if (this.alreadyRevive)
+            return;
+        this.alreadyRevive = true;
+        this.resurrectTime = System.currentTimeMillis();
+        this.deadTime = 0L;
+    }
+
     public UUID getUniqueId() {
         return uniqueId;
+    }
+
+    public boolean wasRevived() {
+        return this.alreadyRevive;
     }
 
     public double getEnderDamage() {
@@ -45,6 +61,8 @@ public class GamePlayer {
     }
 
     public void setDeadTime(final long deadTime) {
+        if (this.wasRevived())
+            this.firstDeathTime = deadTime;
         this.deadTime = deadTime;
     }
 
